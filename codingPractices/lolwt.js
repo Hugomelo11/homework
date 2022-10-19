61ed36d46af40831fb0eca5e7ede195bd4131cc3
Hugo Salles <salles.hugo05@gmail.com>
2022-10-19 08:56:58 -0400

    just practice;

---
+++ codingPractices/lolwt.js
@@ -0,0 +1,9 @@
+const topKFrequent = function(words, k) {
+     words = ["i","love","leetcode","i","love","coding"]
+     k = 2
+    let numOf = words.sort()
+    
+    return numOf
+};
+
+console.log(topKFrequent())
---
+++ codingPractices/morePractice.js
@@ -0,0 +1,23 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// var mergeTwoLists = function(list1, list2) {
+//     list1 = [1,2,4], list2 = [1,3,4]
+//     let fullList = list1.concat(list2)
+//     let orderedList = fullList.sort()
+//     return fullList
+// };
+// console.log(mergeTwoLists())
+
+// write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers
+// in the form of a phone numer => returns "(123) 456-7890"
+
+const phoneNumber = (array) => {
+    let arr = [1,2,3,4,5,6,7,8,9,0]
+   let areaCode = let(for i )
+    console.log(`${areaCode} `)
+}
+phoneNumber()
